c4fa16127a7dab0675026bd5b3c99538807b2087
erilsion <erilsion@gmail.com>
2026-03-14 16:44:53 +0900

    feat: NpcData 스크립터블오브젝트 스크립트 추가

---
+++ Assets/02.Scripts/Npc/ScriptableObject/NpcData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NpcData", menuName = "Scriptable Objects/Npc/NpcData")]
+public class NpcData : ScriptableObject
+{
+    public string NpcId;
+    public string NpcName;
+}
