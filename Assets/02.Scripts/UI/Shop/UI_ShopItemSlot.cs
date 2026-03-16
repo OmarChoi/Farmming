2eda6032d221ffb6b5fd49498c069a1923f4e590
erilsion <erilsion@gmail.com>
2026-03-16 18:11:29 +0900

    feat: 상점 UI 구성

---
+++ Assets/02.Scripts/UI/Shop/UI_ShopItemSlot.cs
@@ -0,0 +1,50 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UI_ShopItemSlot : MonoBehaviour
+{
+    [SerializeField] private Image _icon;
+    [SerializeField] private TextMeshProUGUI _itemNameText;
+    [SerializeField] private TextMeshProUGUI _itemCostText;
+    [SerializeField] private TextMeshProUGUI _itemExplanationText;
+
+    private UI_Shop _uiShop;
+    private int _slotIndex;
+    private ItemDataSO _itemData;
+
+    public int SlotIndex => _slotIndex;
+    public ItemDataSO ItemData => _itemData;
+
+    public void Init(UI_Shop uiShop, int index)
+    {
+        _uiShop = uiShop;
+        _slotIndex = index;
+    }
+
+    public void Refresh(ItemDataSO item)
+    {
+        _itemData = item;
+
+        if (item == null)
+        {
+            _icon.enabled = false;
+            _itemNameText.text = "";
+            _itemCostText.text = "";
+            _itemExplanationText.text = "";
+            return;
+        }
+
+        _icon.enabled = true;
+        _icon.sprite = item.Icon;
+        _itemNameText.text = "";
+        _itemCostText.text = "";
+        _itemExplanationText.text = "";
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        _uiShop.OnShopSlotClicked(this);
+    }
+}
