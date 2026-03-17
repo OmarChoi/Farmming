ec387276d3d0989d2ed9bb38f4af2d7eef841b49
hmssojeong <sojeong9638@gmail.com>
2026-03-17 17:34:13 +0900

    feat: 곡룡별 어빌리티 추가

---
+++ Assets/02.Scripts/Helper/HelperGrade.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public enum EHelperGrade
+{
+    Normal,
+    Epic,
+    Legendary
+}
+public class HelperGrade
+{
+    private HelperDataSO _data;
+
+    public EHelperGrade CurrentGrade { get; private set; }
+
+    public HelperGrade(HelperDataSO data)
+    {
+        _data = data;
+    }
+
+    public int GetRange()
+    {
+        return CurrentGrade switch
+        { EHelperGrade.Normal=> _data.NormalRange,
+          EHelperGrade.Epic => _data.EpicRange,
+          EHelperGrade.Legendary => _data.LegendaryRange,
+          _ => 1
+        };
+    }
+
+    public void Upgrade()
+    {
+        if(CurrentGrade == EHelperGrade.Legendary)
+        {
+            return;
+        }
+        CurrentGrade = (EHelperGrade)((int)CurrentGrade + 1);
+    }
+}
---
+++ Assets/02.Scripts/Helper/Interface/IHelper.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface IHelper
+{
+    EHelperState State { get; }
+}
