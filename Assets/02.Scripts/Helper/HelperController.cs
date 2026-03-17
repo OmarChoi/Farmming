b2ed7edbeccaeff418096a2c18f4dcd38684814d
hmssojeong <sojeong9638@gmail.com>
2026-03-17 18:22:52 +0900

    feat: 개간곡룡 데이터 추가 및 에너지 등급 컨트롤러에 추가

--- Assets/02.Scripts/Helper/HelperController.cs
+++ Assets/02.Scripts/Helper/HelperController.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class HelperController : MonoBehaviour
 {
+    [SerializeField] private HelperDataSO _data;
+
     public EHelperState State { get; private set; } = EHelperState.Summoned;
     public Transform FollowTarget { get; private set; }
 
+    public HelperLevel Level { get; private set; }
+    public HelperGrade Grade { get; private set; }
+    public HelperEnergy Energy { get; private set; }
+
     private readonly Dictionary<Type, HelperAbility> _abilityCache = new();
+
+    private void Awake()
+    {
+        Level = new HelperLevel(_data);
+        Grade = new HelperGrade(_data);
+        Energy = new HelperEnergy(_data);
+
+        Energy.OnExhausted += () => Debug.Log("에너지 소진");
+        Energy.OnRecovered += () => Debug.Log("에너지 회복");
+    }
+
+    private void Update()
+    {
+        Energy.Recover(Time.deltaTime);
+    }
 
     public T GetAbility<T>() where T : HelperAbility
     {
@@ -48,8 +69,18 @@ public class HelperController : MonoBehaviour
 
     public void Interact(TerrainCell cell)
     {
+
+        if(Energy.IsExhausted)
+        {
+            Debug.Log("에너지 소진상태");
+            return;
+        }
+
         var action = GetComponentInChildren<IHelperAction>();
         if (action != null)
+        {
             action.Interact(cell);
+            Energy.TryConsume(Level.GetEnergyCost());
+        }
     }
 }
