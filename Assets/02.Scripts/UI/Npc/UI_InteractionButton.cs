180e1e1f6c4f8731cd2e761a841bacb4d3ceaf32
erilsion <erilsion@gmail.com>
2026-03-17 18:47:07 +0900

    feat: npc 상호작용 및 대화 기능 추가

--- Assets/02.Scripts/UI/Npc/UI_InteractionButton.cs
+++ Assets/02.Scripts/UI/Npc/UI_InteractionButton.cs
@@ -8,9 +8,16 @@ public class UI_InteractionButton : MonoBehaviour
     [SerializeField] private Button _button;
     [SerializeField] private TextMeshProUGUI _interactionText;
 
+    [SerializeField] private Image _background;
+    [SerializeField] private Image _place;
+
     public void Init(string buttonText, Action onClick)
     {
         _interactionText.text = buttonText;
+        _button.enabled = true;
+
+        _background.enabled = true;
+        _place.enabled = true;
 
         _button.onClick.RemoveAllListeners();
         _button.onClick.AddListener(() => onClick?.Invoke());
