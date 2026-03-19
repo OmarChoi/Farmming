a3a6de17f7d8cc1d6f875655bdaf720b048de0a3
hmssojeong <sojeong9638@gmail.com>
2026-03-19 11:04:32 +0900

    chore: helpercontroller에서 helperdataso 변수명 수정

--- Assets/02.Scripts/Helper/HelperController.cs
+++ Assets/02.Scripts/Helper/HelperController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class HelperController : MonoBehaviour
 {
     [SerializeField] private HelperDataSO _data;
-    public HelperDataSO DataSO => _data;
+    public HelperDataSO Data => _data;
     public EHelperState State { get; private set; } = EHelperState.Summoned;
     public Transform FollowTarget { get; private set; }
 
--- Assets/02.Scripts/Helper/HelperTypeAbility/WoodCuttingMineActionAbility.cs
+++ Assets/02.Scripts/Helper/HelperTypeAbility/WoodCuttingMineActionAbility.cs
@@ -18,7 +18,7 @@ public class WoodCuttingMineActionAbility : HelperAbility, IHelperAction
         // 이펙트 소환
         GameObject _effect = Instantiate(_effectWoodPrefab, _effectSpawnPoint.position, _effectSpawnPoint.rotation);
         WoodCuttingVFX cuttingVfx = _effect.GetComponent<WoodCuttingVFX>();
-        cuttingVfx.Initiate(_owner.DataSO.GatherDamage, EGatherType.Wood);
+        cuttingVfx.Initiate(_owner.Data.GatherDamage, EGatherType.Wood);
     }
 
     public void InteractL(TerrainCell cell)
@@ -26,6 +26,6 @@ public class WoodCuttingMineActionAbility : HelperAbility, IHelperAction
         // 이펙트 소환
         GameObject _effect = Instantiate(_effectStonePrefab, _effectSpawnPoint.position, _effectSpawnPoint.rotation);
         WoodCuttingVFX cuttingVfx = _effect.GetComponent<WoodCuttingVFX>();
-        cuttingVfx.Initiate(_owner.DataSO.GatherDamage, EGatherType.Stone);
+        cuttingVfx.Initiate(_owner.Data.GatherDamage, EGatherType.Stone);
     }
 }
