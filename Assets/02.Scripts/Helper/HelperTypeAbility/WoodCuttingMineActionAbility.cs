5d466aaac20887af40877012f1fb108f002bc6c8
hmssojeong <sojeong9638@gmail.com>
2026-03-19 18:20:41 +0900

    feat: 벌목 곡룡 기능 및 애니메이션 구현

--- Assets/02.Scripts/Helper/HelperTypeAbility/WoodCuttingMineActionAbility.cs
+++ Assets/02.Scripts/Helper/HelperTypeAbility/WoodCuttingMineActionAbility.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 // 벌목 채굴: 좌클릭(벌목) / 우클릭(채굴)
@@ -12,13 +13,20 @@ public class WoodCuttingMineActionAbility : HelperAbility, IHelperAction
     [SerializeField] protected GameObject _effectWoodPrefab;
     [SerializeField] protected GameObject _effectStonePrefab;
     [SerializeField] protected Transform _effectSpawnPoint;
+    [SerializeField] private float _idleTransition = 0.5f;
 
     private StoneMineAbility _stoneMineAbility;
+    private HelperAnimationAbility _animAbility;
 
     protected override void Awake()
     {
         base.Awake();
         _stoneMineAbility = _owner.GetAbility<StoneMineAbility>();
+    }
+
+    private void Start()
+    {
+        _animAbility = _owner.GetAbility<HelperAnimationAbility>();
     }
 
     public void Interact(TerrainCell cell)
@@ -28,10 +36,20 @@ public class WoodCuttingMineActionAbility : HelperAbility, IHelperAction
 
     public void InteractPrimary(TerrainCell cell)
     {
+        _animAbility?.Play(EHelperAnim.WoodCutting);
+
         // 이펙트 소환
         GameObject _effect = Instantiate(_effectWoodPrefab, _effectSpawnPoint.position, _effectSpawnPoint.rotation);
         WoodCuttingVFX cuttingVfx = _effect.GetComponent<WoodCuttingVFX>();
         cuttingVfx.Initiate(_owner.Data.GatherDamage, EGatherType.Wood);
+
+        StartCoroutine(AnimPlayCoroutine());
+    }
+
+    public IEnumerator AnimPlayCoroutine()
+    {
+        yield return new WaitForSeconds(_idleTransition);
+        _animAbility?.Play(EHelperAnim.Idle);
     }
 
     public void InteractSecondary(TerrainCell cell)
