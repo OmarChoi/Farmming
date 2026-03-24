4715732dd4f3de0e041a8336e5ba6ef54ee925b5
OmarChoi <omarchoi80@gmail.com>
2026-03-25 03:27:16 +0900

    feat: 건물 건설 완료 시점에 관련 NPC 소환하게 연결

--- Assets/02.Scripts/InGame/Building/Data/BuildingDataSO.cs
+++ Assets/02.Scripts/InGame/Building/Data/BuildingDataSO.cs
@@ -3,17 +3,26 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BuildingData", menuName = "Scriptable Objects/Building Data")]
 public class BuildingDataSO : ScriptableObject
 {
+    [Header("Building Data")]
     [SerializeField] private string _buildingId;
     [SerializeField] private string _displayName;
+    [SerializeField] private Sprite _icon;
+    
+    [Header("Construction Data")]
     [SerializeField] private int _width = 2;
     [SerializeField] private int _depth = 2;
     [SerializeField] private int _constructionDays = 0;
-    [SerializeField] private Sprite _icon;
+    
+    [Header("Npc Data")]
+    [SerializeField] private NpcDataSO _npcDataData;
 
     public string BuildingId => _buildingId;
     public string DisplayName => _displayName;
+    public Sprite Icon => _icon;
+    
     public int Width => _width;
     public int Depth => _depth;
     public int ConstructionDays => _constructionDays;
-    public Sprite Icon => _icon;
+    
+    public NpcDataSO RelatedNpcData => _npcDataData;
 }
