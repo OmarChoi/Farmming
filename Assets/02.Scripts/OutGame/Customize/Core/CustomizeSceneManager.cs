34b938eb4344ac2296146c5c1a3ef3f61ab98d23
hollo112 <76999794+hollo112@users.noreply.github.com>
2026-03-24 12:14:58 +0900

    refactor: SO 일반 클래스로 변경

--- Assets/02.Scripts/OutGame/Customize/Core/CustomizeSceneManager.cs
+++ Assets/02.Scripts/OutGame/Customize/Core/CustomizeSceneManager.cs
@@ -4,12 +4,11 @@ using UnityEngine.SceneManagement;
 public class CustomizeSceneManager : MonoBehaviour
 {
     [SerializeField] private CharacterPartSwapper _partSwapper;
-    [SerializeField] private CustomizeData _customizeData;
     [SerializeField] private string _gameSceneName = "GameScene";
 
     public void OnConfirm()
     {
-        _customizeData.CopyFrom(_partSwapper);
+        CustomizeData.Instance.CopyFrom(_partSwapper);
         SceneManager.LoadScene(_gameSceneName);
     }
 }
