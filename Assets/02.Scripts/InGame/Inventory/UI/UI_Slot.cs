7851e10470bd4f7bfab9e77ffff303ef4e16fada
hollo112 <76999794+hollo112@users.noreply.github.com>
2026-03-24 15:49:22 +0900

    feat: 드래그로 아이템 칸 이동

--- Assets/02.Scripts/InGame/Inventory/UI/UI_Slot.cs
+++ Assets/02.Scripts/InGame/Inventory/UI/UI_Slot.cs
@@ -3,8 +3,7 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
 
-public class UI_Slot : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler,
-    IPointerDownHandler, IPointerUpHandler
+public class UI_Slot : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
     [SerializeField] private Image _icon;
     [SerializeField] private TextMeshProUGUI _countText;
@@ -12,9 +11,6 @@ public class UI_Slot : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
     private UI_Inventory _uiInventory;
     private int _slotIndex;
     private ItemDataSO _currentItem;
-
-    private bool _isPointerDown;
-    private float _pointerDownTime;
 
     public int SlotIndex => _slotIndex;
     public ItemDataSO CurrentItem => _currentItem;
@@ -48,28 +44,10 @@ public class UI_Slot : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
         _countText.text = visible && _currentItem != null ? _countText.text : "";
     }
 
-    private void Update()
-    {
-        if (!_isPointerDown) return;
-
-        if (Time.unscaledTime - _pointerDownTime >= _uiInventory.HoldDuration)
-        {
-            _isPointerDown = false;
-            _uiInventory.BeginDrag(this);
-        }
-    }
-
     public void OnPointerDown(PointerEventData eventData)
     {
         if (_currentItem == null) return;
-        _isPointerDown = true;
-        _pointerDownTime = Time.unscaledTime;
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        _isPointerDown = false;
-        _uiInventory.EndDrag();
+        _uiInventory.BeginDrag(this);
     }
 
     public void OnPointerClick(PointerEventData eventData)
