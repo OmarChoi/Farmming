4b77755fcbc1df5fc5299660785f971fe0474756
hmssojeong <sojeong9638@gmail.com>
2026-03-26 15:27:41 +0900

    feat: test씬 용암타일 생성 추가

--- Assets/02.Scripts/InGame/Helper/Tests/TestLavaTilePlacer.cs
+++ Assets/02.Scripts/InGame/Helper/Tests/TestLavaTilePlacer.cs
@@ -2,28 +2,36 @@ using UnityEngine;
 
 public class TestLavaTilePlacer : MonoBehaviour
 {
-    [SerializeField] private GameObject _lavaTilePrefab;
     [SerializeField] private int _width = 5;
     [SerializeField] private int _height = 5;
-    [SerializeField] private float _cellSize = 2f;
 
     private void Start()
     {
         PlaceLavaTiles();
     }
 
-    public void PlaceLavaTiles()
+    private void PlaceLavaTiles()
     {
-        for (int i = transform.childCount - 1; i >= 0; i--)
-            DestroyImmediate(transform.GetChild(i).gameObject);
+        if (TerrainGridManager.Instance == null)
+        {
+            return;
+        }
 
         for (int x = 0; x < _width; x++)
         {
             for (int z = 0; z < _height; z++)
             {
-                Vector3 pos = new Vector3(x * _cellSize, 0, z * _cellSize);
-                GameObject tile = Instantiate(_lavaTilePrefab, pos, Quaternion.identity, transform);
-                tile.name = $"LavaTile_{x}_{z}";
+                var pos = new Vector3Int(x, 0, z);
+                var data = new TerrainCellData(
+                    ECellType.Dirt,
+                    ETileType.Dungeon2Lava,
+                    1,
+                    EGridObjectType.None,
+                    0,
+                    false,
+                    isTop: true
+                );
+                TerrainGridManager.Instance.SetCell(pos, data);
             }
         }
 
