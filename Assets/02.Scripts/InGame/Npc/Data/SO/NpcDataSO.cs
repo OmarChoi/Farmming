63dc1cc70f95c5db75c37e61da5a8abfc8cde03b
erilsion <erilsion@gmail.com>
2026-03-26 15:05:23 +0900

    feat: ai 프롬프트 관련 DTO 생성과 프롬프트 빌더 클래스 생성

--- Assets/02.Scripts/InGame/Npc/Data/SO/NpcDataSO.cs
+++ Assets/02.Scripts/InGame/Npc/Data/SO/NpcDataSO.cs
@@ -7,6 +7,14 @@ public class NpcDataSO : ScriptableObject
     public string NpcId;
     public string NpcName;
 
+    [Header("AI 대화 가능 여부")]
+    public bool CanUseAiDialogue = true;
+
+    [Header("AI 대화 성격 정보")]
+    public string NpcMbti;
+    public string NpcPersonality;
+    public string Job;
+
     [Header("프리팹")]
     public GameObject Prefab;
 
@@ -16,7 +24,7 @@ public class NpcDataSO : ScriptableObject
     public float JumpDuration;
     public float JumpHeight;
     
-    [Header("대화")]
+    [Header("로컬 대화")]
     public NpcDialogueSO[] GreetDialogues;
     public NpcDialogueSO[] TalkDialogues;
 
