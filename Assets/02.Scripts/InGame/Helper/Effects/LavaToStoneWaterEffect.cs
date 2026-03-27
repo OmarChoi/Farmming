79370a87165710a00560d7c9f772f21c65e93bde
hmssojeong <sojeong9638@gmail.com>
2026-03-27 13:33:55 +0900

    refactor: 용암 연기 시간 단축 수정

--- Assets/02.Scripts/InGame/Helper/Effects/LavaToStoneWaterEffect.cs
+++ Assets/02.Scripts/InGame/Helper/Effects/LavaToStoneWaterEffect.cs
@@ -4,15 +4,12 @@ public class LavaToStoneWaterEffect : IWaterEffect
 {
     public bool CanHandle(TerrainCell cell)
     {
-        Debug.Log($"TileType: {cell.Data.TileType}");
         return cell.Data.TileType == ETileType.Dungeon2Lava;
     }
 
     public void Apply(TerrainCell cell)
     {
-        Debug.Log("Apply 호출됨");
         LavaTileTransition transition = cell?.GetComponent<LavaTileTransition>();
-        Debug.Log($"Transition 찾음: {transition != null}");
 
         if (transition == null) return;
         transition.StartTransition(cell);
