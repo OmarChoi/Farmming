796442c81e5b550ff53411b16635d68843050449
erilsion <erilsion@gmail.com>
2026-03-29 22:34:23 +0900

    refactor: rag 시스템을 가중치 형식으로 변경하여 로직 개선

--- Assets/02.Scripts/InGame/Npc/Data/Memory/NpcMemoryRagDocument.cs
+++ Assets/02.Scripts/InGame/Npc/Data/Memory/NpcMemoryRagDocument.cs
@@ -8,4 +8,9 @@ public class NpcMemoryRagDocument
     public string PlayerId;
     public EMemoryCategory Category;
     public string Content;
+
+    public int Priority;
+    public long UpdatedAtTicks;
+
+    public float[] Embedding;
 }
