057c09ea85896fd38a933c0ef052fc20938e3dd2
hollo112 <76999794+hollo112@users.noreply.github.com>
2026-03-30 10:59:09 +0900

    chore: 곡룡 ability 루트오브젝트로 이동

--- Assets/02.Scripts/InGame/Player/Abilities/PlayerHelperInteractionAbility.cs
+++ Assets/02.Scripts/InGame/Player/Abilities/PlayerHelperInteractionAbility.cs
@@ -23,11 +23,8 @@ public class PlayerHelperInteractionAbility : PlayerAbility
 
     private void OnDestroy()
     {
-        if (_currentHelper == null) return;
-
-        _currentHelper.OnActionStarted -= OnHelperActionStarted;
-        _currentHelper.OnActionEnded -= OnHelperActionEnded;
-        _currentHelper = null;
+        Unsummon();
+        UnsummonBack();
     }
 
     private void Update()
