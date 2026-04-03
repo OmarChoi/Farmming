6c8cc545001e0f55271a24126403b7026e0a8978
hollo112 <76999794+hollo112@users.noreply.github.com>
2026-04-03 16:59:46 +0900

    feat: 지형 생성 후 플레이어 보이게

--- Assets/02.Scripts/InGame/Player/Abilities/PlayerCameraAbility.cs
+++ Assets/02.Scripts/InGame/Player/Abilities/PlayerCameraAbility.cs
@@ -77,6 +77,20 @@ public class PlayerCameraAbility : PlayerAbility
         cinemachineCamera.PreviousStateIsValid = false;
     }
 
+    public void SuspendFollowCamera()
+    {
+        if (!_owner.IsMine) return;
+
+        var followCamObj = GameObject.Find("FollowCamera");
+        if (followCamObj == null) return;
+
+        var cinemachineCamera = followCamObj.GetComponent<CinemachineCamera>();
+        if (cinemachineCamera == null) return;
+
+        cinemachineCamera.Follow = null;
+        cinemachineCamera.PreviousStateIsValid = false;
+    }
+
     public void SetPreset(CameraPreset preset, Transform target = null)
     {
         _preset.data = preset;
