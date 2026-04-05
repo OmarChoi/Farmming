8b925feda25282edf9d63918ad232c43f64e6bf5
OmarChoi <omarchoi80@gmail.com>
2026-04-06 03:00:46 +0900

    feat: 마을 레벨 표시를 위한 UI 추가

---
+++ Assets/02.Scripts/InGame/Village/UI_VillageLevel.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_VillageLevel : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _levelText;
+    [SerializeField] private Image _fillImage;
+
+    private void Start()
+    {
+        VillageLevelManager.Instance.OnVillageStateChanged += UpdateGauge;
+        UpdateGauge();
+    }
+
+    private void OnDestroy()
+    {
+        if (VillageLevelManager.Instance != null)
+        {
+            VillageLevelManager.Instance.OnVillageStateChanged -= UpdateGauge;
+        }
+    }
+    
+    private void UpdateGauge()
+    {
+        int level = VillageLevelManager.Instance.CurrentLevel;
+        int currentGauge = VillageLevelManager.Instance.CurrentGauge;
+        int totalGauge = VillageLevelManager.Instance.CurrentThreshold;
+        
+        _fillImage.fillAmount = (float)currentGauge / totalGauge;
+        _levelText.text = $"LV.{level.ToString().PadLeft(2, '0')}";
+    }
+}
