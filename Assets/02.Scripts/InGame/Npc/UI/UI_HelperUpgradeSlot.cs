db59f1d3abd0af90782183cbec45bdeadb7e3e52
erilsion <erilsion@gmail.com>
2026-04-06 16:09:44 +0900

    feat: npc에 업그레이드 기능 연결

---
+++ Assets/02.Scripts/InGame/Npc/UI/UI_HelperUpgradeSlot.cs
@@ -0,0 +1,72 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_HelperUpgradeSlot : MonoBehaviour
+{
+    [Header("텍스트")]
+    [SerializeField] private TextMeshProUGUI _helperNameText;
+    [SerializeField] private TextMeshProUGUI _helperGradeText;
+    [SerializeField] private TextMeshProUGUI _helperReadyText;
+
+    [Header("선택 표시")]
+    [SerializeField] private GameObject _selectedHighlight;
+
+    [Header("버튼")]
+    [SerializeField] private Button _selectButton;
+
+    private UI_HelperUpgrade _uiHelperUpgrade;
+    private int _slotIndex;
+    private HelperController _helper;
+
+    public int SlotIndex => _slotIndex;
+    public HelperController Helper => _helper;
+
+    private void Awake()
+    {
+        if (_selectButton != null)
+        {
+            _selectButton.onClick.RemoveAllListeners();
+            _selectButton.onClick.AddListener(HandleClickSlot);
+        }
+    }
+
+    public void Init(UI_HelperUpgrade uiHelperUpgrade, int index)
+    {
+        _uiHelperUpgrade = uiHelperUpgrade;
+        _slotIndex = index;
+    }
+
+    public void Refresh(HelperController helper, bool isSelected)
+    {
+        _helper = helper;
+
+        if (helper == null)
+        {
+            _helperNameText.text = "";
+            _helperGradeText.text = "";
+            _helperReadyText.text = "";
+            SetSelected(false);
+            return;
+        }
+
+        _helperNameText.text = helper.HelperId;
+        _helperGradeText.text = helper.Grade.CurrentGrade.ToString();
+        _helperReadyText.text = helper.Experience.IsReadyToUpgrade ? "업그레이드 가능" : "업그레이드 불가";
+
+        SetSelected(isSelected);
+    }
+
+    public void SetSelected(bool isSelected)
+    {
+        if (_selectedHighlight != null)
+        {
+            _selectedHighlight.SetActive(isSelected);
+        }
+    }
+
+    private void HandleClickSlot()
+    {
+        _uiHelperUpgrade?.SelectSlot(_slotIndex);
+    }
+}
