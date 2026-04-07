d94589e17880038b7987b8bb4dfe348555dad5ba
hollo112 <76999794+hollo112@users.noreply.github.com>
2026-04-07 12:46:09 +0900

    refactor: 리팩토링

--- Assets/02.Scripts/InGame/Core/LoadingProgressUI.cs
+++ Assets/02.Scripts/InGame/Core/LoadingProgressUI.cs
@@ -1,4 +1,3 @@
-using Photon.Pun;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,10 +9,6 @@ public class LoadingProgressUI : MonoBehaviour
     [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private float _smoothSpeed = 5f;
     [SerializeField] private float _fadeOutSpeed = 2f;
-
-    private float _sceneLoadBase;
-    private float _sceneLoadRange;
-    private bool _trackingSceneLoad;
 
     private void Awake()
     {
@@ -30,30 +25,9 @@ public class LoadingProgressUI : MonoBehaviour
             _canvasGroup.alpha = 1f;
     }
 
-    /// <summary>
-    /// 씬 로딩 진행률 추적 시작.
-    /// base ~ base+range 범위를 PhotonNetwork.LevelLoadingProgress로 채움.
-    /// </summary>
-    public void TrackSceneLoad(float baseValue, float range)
-    {
-        _sceneLoadBase = baseValue;
-        _sceneLoadRange = range;
-        _trackingSceneLoad = true;
-    }
-
     private void Update()
     {
-        if (_trackingSceneLoad && PhotonNetwork.IsConnected)
-        {
-            float sceneProgress = PhotonNetwork.LevelLoadingProgress;
-            float mapped = _sceneLoadBase + _sceneLoadRange * sceneProgress;
-
-            if (mapped > LoadingProgress.Value)
-                LoadingProgress.Value = mapped;
-
-            if (sceneProgress >= 1f)
-                _trackingSceneLoad = false;
-        }
+        LoadingProgress.Tick();
 
         if (_fillBar != null)
         {
