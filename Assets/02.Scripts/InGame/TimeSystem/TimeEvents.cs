31075316dae7b96c3c7aab2be472c86ec2292c27
Omarchoi <omarchoi80@gmail.com>
2026-04-10 18:38:17 +0900

    feat: 이미지 기반 UI로 수정

--- Assets/02.Scripts/InGame/TimeSystem/TimeEvents.cs
+++ Assets/02.Scripts/InGame/TimeSystem/TimeEvents.cs
@@ -15,6 +15,7 @@ public static class TimeEvents
     public static GameTime CurrentTime { get; private set; }
     public static bool IsDayTime { get; private set; }
     public static int ElapsedDays { get; private set; }
+    public static TimeSettingSO Settings { get; private set; }
 
     public static event Action<GameTime> OnMinuteChanged;
 
@@ -36,6 +37,12 @@ public static class TimeEvents
         IsDayTime = isDayTime;
         ElapsedDays = elapsedDays;
     }
+
+    internal static void SetSpecData(TimeSettingSO timeSetting)
+    {
+        Settings = timeSetting;
+    }
+    
     internal static void InvokeMinuteChanged(GameTime time) => OnMinuteChanged?.Invoke(time);
     internal static void InvokeNetDayChanged() => OnNetDayChanged?.Invoke();
     internal static void InvokeNetDayStarted() => OnNetDayStarted?.Invoke();
