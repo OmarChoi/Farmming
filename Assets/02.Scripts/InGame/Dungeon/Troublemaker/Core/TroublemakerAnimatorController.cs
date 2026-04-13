da252500e95697966145bc798ac48000a4a7aac2
erilsion <erilsion@gmail.com>
2026-04-13 13:20:31 +0900

    feat: TroublemakerController 생성 및 행동 관련 클래스 생성

---
+++ Assets/02.Scripts/InGame/Dungeon/Troublemaker/Core/TroublemakerAnimatorController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class TroublemakerAnimatorController : MonoBehaviour, IMovementAnimator
+{
+    [Header("애니메이터")]
+    [SerializeField] private Animator _animator;
+
+    [Header("애니메이터 매개변수 명칭 (최대한 통일)")]
+    [SerializeField] private string _moveFloatName = "Move";
+    [SerializeField] private string _detectTriggerName = "Detect";
+    [SerializeField] private string _troubleTriggerName = "Trouble";
+
+    private int _moveHash;
+    private int _detectHash;
+    private int _troubleHash;
+
+    private void Awake()
+    {
+        if (_animator == null)
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        _moveHash = Animator.StringToHash(_moveFloatName);
+        _detectHash = Animator.StringToHash(_detectTriggerName);
+        _troubleHash = Animator.StringToHash(_troubleTriggerName);
+    }
+
+    public void SetMove(float value)
+    {
+        if (_animator == null) return;
+        _animator.SetFloat(_moveHash, value);
+    }
+
+    public void PlayDetect()
+    {
+        if (_animator == null) return;
+        _animator.SetTrigger(_detectHash);
+    }
+
+    public void PlayTrouble()
+    {
+        if (_animator == null) return;
+        _animator.SetTrigger(_troubleHash);
+    }
+}
---
+++ Assets/02.Scripts/InGame/Npc/Interfaces/IMovementAnimator.cs
@@ -0,0 +1,5 @@
+
+public interface IMovementAnimator
+{
+    void SetMove(float moveSpeed);
+}
