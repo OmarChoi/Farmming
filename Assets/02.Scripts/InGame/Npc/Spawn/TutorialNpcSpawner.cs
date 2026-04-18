a23be0c427d7fecac5b3d74bd7270b1d785a6a16
erilsion <erilsion@gmail.com>
2026-04-18 15:22:28 +0900

    feat: 튜토리얼 초반부에 이동 관련 내용 추가

--- Assets/02.Scripts/InGame/Npc/Spawn/TutorialNpcSpawner.cs
+++ Assets/02.Scripts/InGame/Npc/Spawn/TutorialNpcSpawner.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public static class TutorialNpcSpawner
 {
-    private const float SpawnRadius = 3.8f;
+    [Header("스폰 범위 설정")]
+    [SerializeField] private static float _spawnRadius = 3.8f;
+    [SerializeField] private static float _firstTutorialSpawnRadius = 8.2f;
 
-    public static NpcController SpawnNearPlayer(NpcDataSO data, Transform player)
+    private static ETutorialState _state;
+
+    public static NpcController SpawnNearPlayer(NpcDataSO data, PlayerController player)
     {
         if (data == null || player == null || NpcSpawnManager.Instance == null) return null;
 
-        Vector3 spawnPosition = player.position + GetRandomOffset();
+        _state = player.GetAbility<PlayerQuestAbility>()?.TutorialState ?? ETutorialState.None;
+
+        Vector3 spawnPosition = player.transform.position + GetRandomOffset(SetSpawnRadius(_state));
 
         var request = new NpcSpawnRequest(
             data,
@@ -22,9 +28,22 @@ public static class TutorialNpcSpawner
         return NpcSpawnManager.Instance.GetOrSpawn(request);
     }
 
-    private static Vector3 GetRandomOffset()
+    private static float SetSpawnRadius(ETutorialState state)
     {
-        Vector2 circle = Random.insideUnitCircle * SpawnRadius;
+        switch(state){
+            case ETutorialState.None:
+                return _firstTutorialSpawnRadius;
+            case ETutorialState.InProgress:
+            case ETutorialState.Completed:
+                return _spawnRadius;
+            default:
+                return _spawnRadius;
+        }
+    }
+
+    private static Vector3 GetRandomOffset(float radius)
+    {
+        Vector2 circle = Random.insideUnitCircle * radius;
         return new Vector3(circle.x, 0f, circle.y);
     }
 }
