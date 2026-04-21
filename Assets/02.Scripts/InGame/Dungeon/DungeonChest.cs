e44f0f68207715e17f28bc186192932f8546e0bb
hollo112 <76999794+hollo112@users.noreply.github.com>
2026-04-21 16:41:07 +0900

    feat: 상자 획득시 소리, 이펙트 추가

--- Assets/02.Scripts/InGame/Dungeon/DungeonChest.cs
+++ Assets/02.Scripts/InGame/Dungeon/DungeonChest.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class DungeonChest : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private GameObject _openParticleRoot;
 
     private ChestLootTable _lootTable;
     private bool _opened;
@@ -26,7 +27,25 @@ public class DungeonChest : MonoBehaviour
         if (_animator != null)
             _animator.SetTrigger(OpenHash);
 
+        PlayOpenEffects();
         GiveLoot(player);
+    }
+
+    private void PlayOpenEffects()
+    {
+        if (_openParticleRoot != null)
+        {
+            _openParticleRoot.SetActive(false);
+            _openParticleRoot.SetActive(true);
+        }
+
+        if (SoundManager.Instance != null)
+        {
+            SoundManager.Instance.PlaySfx(new SfxPlayRequest(
+                clipKey: AssetKey.SFX.ChestOpen,
+                spatialMode: ESpatialMode.Positional3D,
+                position: transform.position));
+        }
     }
 
     private void GiveLoot(PlayerController player)
