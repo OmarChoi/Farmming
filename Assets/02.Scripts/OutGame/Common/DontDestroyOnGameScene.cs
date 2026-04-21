53c84e7c0ada1dfa03ad0af983825e987203ea6b
Omarchoi <omarchoi80@gmail.com>
2026-04-21 14:08:24 +0900

    fix: Currency 유지 안되는 문제 해결

--- Assets/02.Scripts/OutGame/Common/DontDestroyOnGameScene.cs
+++ Assets/02.Scripts/OutGame/Common/DontDestroyOnGameScene.cs
@@ -1,29 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NpcManager : MonoBehaviour
+public class DontDestroyOnGameScene : MonoBehaviour
 {
-    public static NpcManager Instance { get; private set; }
     private void Awake()
     {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
         DontDestroyOnLoad(gameObject);
-        SceneManager.sceneLoaded -= OnSceneLoaded;
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     private void OnDestroy()
     {
-        if (Instance == this)
-        {
-            Instance = null;
-        }
-        
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
     
