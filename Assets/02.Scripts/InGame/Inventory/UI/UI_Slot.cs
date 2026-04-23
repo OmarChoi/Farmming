957dea9d4b5fe9c9a061abd19b9ef753d87d1da2
hollo112 <76999794+hollo112@users.noreply.github.com>
2026-04-23 20:20:23 +0900

    fix: 물약 드래그 가능하게 변경

--- Assets/02.Scripts/InGame/Inventory/UI/UI_Slot.cs
+++ Assets/02.Scripts/InGame/Inventory/UI/UI_Slot.cs
@@ -49,7 +49,6 @@ public class UI_Slot : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
         if (eventData.button != PointerEventData.InputButton.Left)
             return;
         if (_currentItem == null) return;
-        if (_currentItem is PotionDataSO) return;
 
         bool shift = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
         _container.BeginDrag(this, shift);
